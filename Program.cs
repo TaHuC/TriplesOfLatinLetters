763c59457a356f16e61026272dbba1f3545443e6
TaHuC <TaHuC@hotmail.com>
2017-01-24 23:04:00 +0200

    finish this task

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TriplesOfLatinLetters
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			int n = int.Parse(Console.ReadLine());
+
+			for (int first = 0; first < n; first++)
+			{
+				for (int second = 0; second < n; second++)
+				{
+					for (int last = 0; last < n; last++)
+					{
+						char latter1 = (char)('a' + first);
+						char latter2 = (char)('a' + second);
+						char latter3 = (char)('a' + last);
+
+						Console.WriteLine("{0}{1}{2}", latter1, latter2, latter3);
+					}
+				}
+			}
+		}
+	}
+}
